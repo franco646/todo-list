c3e957df25a0be78e36cccb7267dff46ba3bd0ac
franco646 <franco@>
2021-05-19 19:27:00 -0300

    Update addTask.test.js

--- src/components/addTask/__test__/addTask.test.js
+++ src/components/addTask/__test__/addTask.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, fireEvent, screen, waitFor } from '../../../redux/test-utils';
 import '@testing-library/jest-dom/extend-expect'
 
-import { AddTask } from '../addTask';
+import AddTask from '../addTask';
 
 describe('<AddTask />', () => {
     it('should handle submit on click add button', async () => {
