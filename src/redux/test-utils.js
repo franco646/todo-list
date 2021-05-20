9382c12ef25e4a60b4423cb89fda80f546a4a60c
franco646 <franco@>
2021-05-19 21:52:46 -0300

    Agrega test

--- src/redux/test-utils.js
+++ src/redux/test-utils.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import { render as rtlRender } from '@testing-library/react'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import rootReducer from "./root-reducer";
+import { STATUS_FILTERS, PRIORITY_FILTERS } from "../constants";
 
-function render(
-  ui,
-  {
-    initialState,
-    store = createStore(rootReducer, initialState),
-    ...renderOptions
-  } = {}
-) {
+export const store = createStore(rootReducer, {
+  tasks: {
+    taskList: [
+      {
+        id: 1,
+        priority: PRIORITY_FILTERS.HIGH,
+        status: STATUS_FILTERS.NEW,
+        title: "",
+        content: "",
+      },
+    ],
+  },
+});
+
+function render(ui, renderOptions) {
   function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>{children}</Provider>;
   }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
-export * from '@testing-library/react'
+export * from "@testing-library/react";
 
-export { render }
+export { render };
